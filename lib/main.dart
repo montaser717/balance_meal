e15d04674e6ec236d9d19ec5cc30d05233739eea
bastian <ba121rom@htwg-konstanz.de>
2025-06-24 13:43:41 +0200

    feat: hive added for meal and food item

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,20 @@
-import 'package:balance_meal/router/app_router.dart';
+import 'package:balance_meal/models/food_item.dart';
 import 'package:flutter/material.dart';
-import 'router/app_router.dart';
-import 'common/app_theme.dart'; //
-void main() {
+import 'package:balance_meal/router/app_router.dart';
+import 'package:hive/hive.dart';
+import 'package:path_provider/path_provider.dart';
+import 'common/app_theme.dart';
+import 'models/meal.dart';
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final appDocDir = await getApplicationDocumentsDirectory();
+  Hive.init(appDocDir.path);
+
+  Hive.registerAdapter(MealAdapter());
+  Hive.registerAdapter(FoodItemAdapter());
+
+  await Hive.openBox<Meal>('meals');
+
   runApp(const CalorinTrackerApp());
 }
 
