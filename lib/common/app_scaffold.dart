3d695df3f1872084b8aeb02308b96d99b021d1b6
bastian <ba121rom@htwg-konstanz.de>
2025-06-25 16:26:42 +0200

    refactor

--- lib/common/app_scaffold.dart
+++ lib/common/app_scaffold.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import '../UI/profile/profile_view.dart';
-import 'app_routes.dart';
 
 class AppScaffold extends StatelessWidget {
   final StatefulNavigationShell navigationShell;
